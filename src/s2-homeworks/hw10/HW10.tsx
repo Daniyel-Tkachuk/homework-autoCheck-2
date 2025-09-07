f726df878fbdb518ade47243a2b8aba9d61f60c4
Daniyel-Tkachuk <danilaradyn7357899@gmail.com>
2025-09-07 17:40:55 +0300

    added HW9

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -14,35 +14,36 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
+  const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading)
+  const dispatch = useDispatch()
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+  const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+    dispatch(loadingAC(true))
+    setTimeout(() => {
+      dispatch(loadingAC(false))
+    }, 1500)
+  }
 
-        // setTimeout
-    }
+  return (
+    <div id={'hw10'}>
+      <div className={s2.hwTitle}>Homework #10</div>
 
-    return (
-        <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
-
-            <div className={s2.hw}>
-                {isLoading ? (
-                    <div id={'hw10-loading'}>
-                        <Loader/>
-                    </div>
-                ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
-                )}
-            </div>
-        </div>
-    )
+      <div className={s2.hw}>
+        {isLoading ? (
+          <div id={'hw10-loading'}>
+            <Loader/>
+          </div>
+        ) : (
+          <SuperButton
+            id={'hw10-button-start-loading'}
+            onClick={setLoading}
+          >
+            Set loading...
+          </SuperButton>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default HW10
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,8 @@
 import s from './Loader.module.css'
+import {ReactComponent as LoaderSvg } from './images/loader.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+  <div className={s.loader}>
+    <LoaderSvg className={s.loaderSvg}/>
+  </div>
+)
