531bada628242502ee1f259b0b34773b43bde709
Daniyel-Tkachuk <danilaradyn7357899@gmail.com>
2026-06-20 20:24:40 +0300

    added HW10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,20 @@
+import {AppStoreType} from "./store";
+
+export type InitState = typeof initState
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: InitState = initState, action: LoadingActionType): InitState => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': {
+            return {
+                ...state,
+                isLoading: action.isLoading,
+            }
+        }
         default:
             return state
     }
@@ -20,3 +29,5 @@ export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+export const selectIsLoading = (state: AppStoreType) => state.loading.isLoading
